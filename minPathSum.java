f3c6075e379d7089d947fc18a7c0bb6fedeb9785
feselene <vaporflyb@gmail.com>
2024-10-11 12:49:05 -0400

    check in minPathSum and mininmumSizeSubarraySum.java

---
+++ minPathSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int [][] ans = new int[m][n];
+
+        ans[0][0] = grid[0][0];
+
+        for (int i = 1; i < m; i++) {
+            ans[i][0] = ans[i-1][0] + grid[i][0];
+        }
+
+        for (int j = 1; j < n; j++) {
+            ans[0][j] = ans[0][j-1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                ans[i][j] = Math.min(ans[i-1][j], ans[i][j-1]) + grid[i][j];
+            }
+        }
+
+        return ans[m-1][n-1];
+    }
+}
--- minimumSizeSubarraySum.java
+++ minimumSizeSubarraySum.java
@@ -1,2 +1,18 @@
-public class minimumSizeSubarraySum {
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minSize = Integer.MAX_VALUE;
+        int windowSum = 0;
+        int leftSideOfWindow = 0;
+
+        for (int rightSideOfWindow = 0; rightSideOfWindow < nums.length; rightSideOfWindow++) {
+            windowSum += nums[rightSideOfWindow];
+            if (windowSum >= target) {
+                minSize = Math.min(minSize, rightSideOfWindow - leftSideOfWindow);
+                windowSum -= nums[leftSideOfWindow];
+                leftSideOfWindow++;
+            }
+        }
+
+        return minSize == Integer.MAX_VALUE ? 0 : minSize;
+    }
 }
